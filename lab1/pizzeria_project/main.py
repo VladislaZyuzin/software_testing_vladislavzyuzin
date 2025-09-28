c36629f2e8291fe105ea6685c68ad1feb462d42b
VladislavZyuzin <VLAD_z01@inbox.ru>
2025-09-28 21:23:55 +0300

    Create main.py
    Signed-off-by: VladislavZyuzin <VLAD_z01@inbox.ru>

---
+++ lab1/pizzeria_project/main.py
@@ -0,0 +1,9 @@
+from pizzeria.terminal import Terminal
+
+def main():
+    terminal = Terminal()
+    print("Welcome to the Pizzeria 'Mishka Fredy!'")
+    terminal.take_order()
+
+if __name__ == "__main__":
+    main()
